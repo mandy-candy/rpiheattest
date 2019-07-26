cbb78e863c0b2d4a37492131061fec5da92f1e25
Amanda Kuenzle <amanda.kuenzle@buhlergroup.com>
2019-07-26 14:55:52 +0200

    deleted 4 test to get a shorter runtime to test the time measurement

--- heattest.sh
+++ heattest.sh
@@ -14,28 +14,7 @@ echo "temperature before testing: "
 vcgencmd measure_temp
 echo " "
 echo "executing test 1..."
-sysbench --test=cpu --cpu-max-prime=20000 --num-threads=4 run >/dev/null 2>&1
+time sysbench --test=cpu --cpu-max-prime=5000 --num-threads=4 run >/dev/null 2>&1
 echo "...finished test 1"
 vcgencmd measure_temp
-echo " "
-echo "executing test 2..."
-sysbench --test=cpu --cpu-max-prime=20000 --num-threads=4 run >/dev/null 2>&1
-echo "...finished test 2"
-vcgencmd measure_temp
-echo " "
-echo "executing test 3..."
-sysbench --test=cpu --cpu-max-prime=20000 --num-threads=4 run >/dev/null 2>&1
-echo "...finished test 3"
-vcgencmd measure_temp
-echo " "
-echo "executing test 4..."
-sysbench --test=cpu --cpu-max-prime=20000 --num-threads=4 run >/dev/null 2>&1
-echo "...finished test 4"
-vcgencmd measure_temp
-echo " "
-echo "executing test 5..."
-sysbench --test=cpu --cpu-max-prime=20000 --num-threads=4 run >/dev/null 2>&1
-echo "...finished test 5"
-vcgencmd measure_temp
-echo " "
-echo "all 5 tests are finished"
+
